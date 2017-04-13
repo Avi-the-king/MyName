135aeb770d57f1134fb28766994e56e44434b57b
Avi-the-king <hsnaskna@gmail.com>
2017-04-13 14:15:06 +0530

    Add files via upload

---
+++ MyName.java
@@ -0,0 +1,8 @@
+//program to print name of a person
+  class MyName
+  {
+      public static void main (Strings[]args)
+      {
+          System.out.println('Good Morning,Avi The Great');
+       }
+   }
